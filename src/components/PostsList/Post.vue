2ae83a0d6dae77b42e449282da0b1d1252dcd096
Matthews3301 <salamon.mat@gmail.com>
2024-03-16 22:20:00 +0000

    front-end fixes

--- src/components/PostsList/Post.vue
+++ src/components/PostsList/Post.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { defineProps, ref, watchEffect } from 'vue';
+import { computed, defineProps, ref, watchEffect } from 'vue';
 import Button from '@/atoms/Button/index.vue';
 import Spinner from '@/atoms/Spinner/index.vue';
+import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/solid';
 import type { GraphAdded } from '@/composables/useGraphAddeds';
 import { formatDateTime, truncateAddress } from '@/utils/formatters';
 import { isSameAddress } from '@/utils/web3';
@@ -37,8 +38,8 @@ const { data: ipfsHashString, isLoading } = useGetIpfsHashContent(props.post.ipf
 const { refetch: refetchAccepteds } = useGraphAccepteds();
 
 // COMPUTED
-const isAccepted = props.acceptedList.includes(props.post.ipfsHash);
-const isOwner = isSameAddress(props.address, ownerAddress)
+const isAccepted = computed(() => props.acceptedList.includes(props.post.ipfsHash))
+const isOwner = computed(() => isSameAddress(props.address, ownerAddress))
 
 // WATCHERS
 watchEffect(() => {
@@ -77,20 +78,27 @@ function acceptPost() {
 </script>
 
 <template>
-  <div class="py-4 flex justify-between items-center" :class="{ 'opacity-50': isAccepted }">
-    <div>
-      <p class="text-primary mb-1">
-        {{ truncateAddress(post.proposer) }}
-      </p>
-      <p v-if="isLoading" class="mb-1">Loading...</p>
-      <p v-else class="mb-1">
-        {{ ipfsHashString ?? '-' }}
-      </p>
-      <p class="text-sm text-slate-600 mb-1">
-        {{ formatDateTime(Number(post.blockTimestamp) * 1000) }}
-      </p>
+  <div class="py-4 grid grid-cols-[1fr,200px]" :class="{ 'opacity-50': isAccepted }">
+    <div class="overflow-hidden text-wrap overflow-wrap break-words">
+      <div>
+        <p class="text-primary mb-1">
+          {{ truncateAddress(post.proposer) }}
+        </p>
+        <p v-if="isLoading" class="mb-1">Loading...</p>
+        <p v-else class="mb-1">
+          {{ ipfsHashString ?? '-' }}
+          <a :href="`https://ipfs.io/ipfs/${post.ipfsHash}`" target="_blank" class="ahref">
+            <ArrowTopRightOnSquareIcon
+              class="inline-block w-4 h-4 text-slate-600 ml-1"
+            />
+          </a>
+        </p>
+        <p class="text-sm text-slate-600 mb-1">
+          {{ formatDateTime(Number(post.blockTimestamp) * 1000) }}
+        </p>
+      </div>
     </div>
-    <div>
+    <div class="flex justify-center items-center">
       <Button v-if="isAccepted" type="Light" class="min-w-40" disabled> ✅ Accepted </Button>
       <Button
         v-else-if="status !== 'connected'"
